ec47a6f53831c72df833a2e07db468cd2928ccb9
pavelv <pavelv87@gmail.com>
2021-01-16 13:55:44 +0600

    test actions lint 5

--- src/features/modals/index.js
+++ src/features/modals/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import AddChannelModal from './AddChannelModal.jsx';
 import EditChannelModal from './EditChannelModal.jsx';
