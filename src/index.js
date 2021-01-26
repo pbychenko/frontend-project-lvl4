b46199c6febed7d2047e738a861d9a81a747f471
pavelv <pavelv87@gmail.com>
2021-01-26 08:03:33 +0600

    change init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // // @ts-check
-import { render } from 'react-dom';
+// import { render } from 'react-dom';
 import gon from 'gon';
 import runApp from './init';
 
-render(runApp(gon), document.getElementById('chat'));
+// render(runApp(gon), document.getElementById('chat'));
+runApp(gon);
